25c40d7d67057c6d7c10227d34531c0244b7f417
Ahmad Reza <hungry.dev39@gmail.com>
2020-04-13 20:24:00 +0700

    fix: error without argument

--- cli.js
+++ cli.js
@@ -5,15 +5,17 @@ const githubTrends = require('@huchenme/github-trending');
 const [,, sinceSwitch] = process.argv
 
 let assignedLang = 'javascript'
-let assignedSince;
+let assignedSince = '';
 
-if (sinceSwitch === '-d' || sinceSwitch === '') {
+if (sinceSwitch === '-d') {
   assignedSince = 'daily'
 } else if (sinceSwitch === '-m') {
   assignedSince = 'monthly'
 } else if (sinceSwitch === '' || sinceSwitch === '-w') {
   assignedSince = 'weekly'
 }
+
+console.log(assignedSince);
 
 const capitalize = words =>
   words.charAt(0).toUpperCase() + words.slice(1);
